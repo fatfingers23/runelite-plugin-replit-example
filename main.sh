3df30c2d219f32cd34d2ce44be7cca6a2cfb01d8
Bailey Townsend <baileytownsend2323@gmail.com>
2022-04-22 17:36:21 -0500

    Replit Files

---
+++ main.sh
@@ -0,0 +1,3 @@
+# There is a dependency that does not download properly unless you remove them before build. I'm sure there's a much less of a destructive way to do this.
+rm -rf ~/.m2
+./gradlew --init-script PluginTest.gradle :RunPlugin
---
+++ replit.nix
@@ -0,0 +1,9 @@
+{ pkgs }: {
+    deps = [
+        pkgs.graalvm17-ce
+        pkgs.maven
+        pkgs.replitPackages.jdt-language-server
+        pkgs.replitPackages.java-debug
+        pkgs.gradle_6
+    ];
+}
