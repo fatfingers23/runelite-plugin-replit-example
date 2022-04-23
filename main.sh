cac541f6682b100efd0c382bf4251d67884eecda
Bailey Townsend <baileytownsend2323@gmail.com>
2022-04-23 06:33:02 +0000

    Improvements to make sure it can run on new start up

--- main.sh
+++ main.sh
@@ -1,3 +1,5 @@
 # There is a dependency that does not download properly unless you remove them before build. I'm sure there's a much less of a destructive way to do this.
 rm -rf ~/.m2
+chmod +x ./gradlew 
+./gradlew build --refresh-dependencies
 ./gradlew --init-script PluginTest.gradle :RunPlugin
